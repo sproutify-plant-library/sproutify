c67f3cbc6ee3df859ca8fd2dafc382f3e2f5701b
Rutushree Palwekar <palwekar.rutushree@gmail.com>
2024-08-22 13:03:40 +0200

    add logo and style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,23 @@
 import { Link } from "react-router-dom";
 
-function Header() {
+function Header({ onFilterChange }) {
   return (
-    <header>
-      <Link to={`/`}>
-        <h1>Sproutify 🪴</h1>
-      </Link>
-    </header>
+    <>
+      <header className="header">
+        <Link to={`/`}>
+          <img src="../public/Sproutify_logo.png" alt="Sproutify Logo" />
+        </Link>
+      </header>
+      <nav className="navbar">
+        <ul className="nav-list">
+          <li className="nav-item" onClick={() => onFilterChange("")}>All Plants</li>
+          <li className="nav-item" onClick={() => onFilterChange("indoor")}>Indoor Plants</li>
+          <li className="nav-item" onClick={() => onFilterChange("outdoor")}>Outdoor Plants</li>
+          <li className="nav-item" onClick={() => onFilterChange("succulent")}>Succulents</li>
+          <li className="nav-item" onClick={() => onFilterChange("herb")}>Herbs</li>
+        </ul>
+      </nav>
+    </>
   );
 }
 
