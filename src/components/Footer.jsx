375b23151f11f4ffd2cea9068f0a1b0dfeabf47f
Rutushree Palwekar <palwekar.rutushree@gmail.com>
2024-08-22 18:22:16 +0200

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,6 +6,7 @@ function Footer() {
     return (
       <footer className="footer">
       <p>Copyright © 2024 Sproutify - All rights reserved</p>
+      <p>Daniela  •  Rutushree</p>
     </footer>
   );
 }
