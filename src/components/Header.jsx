f52340adec9a760467404dc76c4b3d7e037fe733
Rutushree Palwekar <palwekar.rutushree@gmail.com>
2024-08-22 18:25:50 +0200

    add create button to navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,11 +10,26 @@ function Header({ onFilterChange }) {
       </header>
       <nav className="navbar">
         <ul className="nav-list">
-          <li className="nav-item" onClick={() => onFilterChange("")}>All Plants</li>
-          <li className="nav-item" onClick={() => onFilterChange("indoor")}>Indoor Plants</li>
-          <li className="nav-item" onClick={() => onFilterChange("outdoor")}>Outdoor Plants</li>
-          <li className="nav-item" onClick={() => onFilterChange("succulent")}>Succulents</li>
-          <li className="nav-item" onClick={() => onFilterChange("herb")}>Herbs</li>
+          <li className="nav-item" onClick={() => onFilterChange("")}>
+            All Plants
+          </li>
+          <li className="nav-item" onClick={() => onFilterChange("indoor")}>
+            Indoor Plants
+          </li>
+          <li className="nav-item" onClick={() => onFilterChange("outdoor")}>
+            Outdoor Plants
+          </li>
+          <li className="nav-item" onClick={() => onFilterChange("succulent")}>
+            Succulents
+          </li>
+          <li className="nav-item" onClick={() => onFilterChange("herb")}>
+            Herbs
+          </li>
+          <li className="nav-item">
+            <Link to="/plant/create">
+              <button className="createButton">Create </button>
+            </Link>
+          </li>
         </ul>
       </nav>
     </>
