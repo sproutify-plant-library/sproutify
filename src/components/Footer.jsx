8fad78676271ef640b1b4c5f86a6ab42f7623159
Rutushree Palwekar <palwekar.rutushree@gmail.com>
2024-09-20 11:13:04 +0200

    add repo link to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 
 function Footer() {
@@ -5,8 +6,8 @@ function Footer() {
 
     return (
       <footer className="footer">
-      <p>Copyright © 2024 Sproutify - All rights reserved</p>
-      <p>Daniela  •  Rutushree</p>
+      <p>Copyright © 2024 <Link to="https://github.com/sproutify-plant-library">Sproutify</Link> - All rights reserved</p>
+      <p> <Link to="https://github.com/daniela-vp">Daniela </Link>  •  <Link to="https://github.com/RutuPalwekar"> Rutushree </Link></p>
     </footer>
   );
 }
